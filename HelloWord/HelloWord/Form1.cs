35761f43b3009d026d0059a37035c157d5629565
franksney <franksneya@gmail.com>
2022-05-06 20:12:47 -0300

    FIM DE TESTE

--- HelloWord/HelloWord/Form1.cs
+++ HelloWord/HelloWord/Form1.cs
@@ -1,25 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace HelloWord
 {
-    public partial class Form1 : Form
+    public partial class FormPrincipal : Form
     {
-        public Form1()
+        public FormPrincipal()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello Word!");
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
